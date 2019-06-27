073dd018a6c579a18cacaecfbb45d9c8d9cc35cc
Zachary Bell <z.bell@me.com>
2019-06-27 19:42:59 +0000

    Add detail Show page for gems

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -18,6 +18,14 @@ class GramsController < ApplicationController
           end
     end
 
+    def show
+        @gram = Gram.find_by_id(params[:id])
+        if @gram.blank?
+            render plain: 'Not Found :(', status: :not_found
+          end
+        
+    end
+
     private
 
     def gram_params
