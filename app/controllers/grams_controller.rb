5988a9cf24b040032f84c7fbdcf907f14cbe1d83
Zachary Bell <z.bell@me.com>
2019-06-27 22:51:05 +0000

    Secure edit, update, and destroy methods for grams by only allowing the creator to use

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,9 +1,10 @@
 class GramsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :destroy, :update]
 
     def destroy
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
         @gram.destroy
         redirect_to root_path
     end
@@ -32,11 +33,14 @@ class GramsController < ApplicationController
     def edit
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
     end
      
     def update
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
+        return render_not_found(:forbidden) if @gram.user != current_user
+            
 
         @gram.update_attributes(gram_params)
         if @gram.valid?
@@ -52,7 +56,7 @@ class GramsController < ApplicationController
         params.require(:gram).permit(:message)
     end
 
-    def render_not_found
-        render plain: 'Not Found :(', status: :not_found
+    def render_not_found(status=:not_found)
+        render plain: "#{status.to_s.titleize}", status: status
     end
 end
