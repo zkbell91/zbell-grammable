3e0a38400d4991bda7bc2e4bd8dacd670c280b4b
Zachary Bell <z.bell@me.com>
2019-06-27 22:09:13 +0000

    Add destroy functionality for grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,8 +1,14 @@
 class GramsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create]
 
-    def index
-        
+    def destroy
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+        @gram.destroy
+        redirect_to root_path
+    end
+
+    def index 
     end
 
     def new
@@ -13,9 +19,9 @@ class GramsController < ApplicationController
         @gram = current_user.grams.create(gram_params)
         if @gram.valid?
             redirect_to root_path
-          else
+        else
             render :new, status: :unprocessable_entity
-          end
+        end
     end
 
     def show
