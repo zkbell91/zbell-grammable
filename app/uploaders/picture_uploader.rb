cbb535052bcdbe08968e39e0aedd345856f79d0c
Zack Bell <Zack@Zacks-MacBook-Air.local>
2019-07-02 19:46:00 -0400

    Adjust Minimagick

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -22,7 +22,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [800, 350]
+  process resize_to_fit: [800, 350]
   #
   # def scale(width, height)
   #   # do something
