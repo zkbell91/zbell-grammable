c4967543a80a95bb6443ab49ea1d1b19325c8cb7
zkbell91 <z.bell@me.com>
2019-07-10 05:41:53 +0000

    remove scaling using miniMagick during image upload

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -22,7 +22,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process scale: [200, 300]
+  # process scale: [200, 300]
   #
   # def scale(width, height)
   #   # do something
